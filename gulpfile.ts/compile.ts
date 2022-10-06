45d6511856b390cef9fa85d9f18215f82e17735d
Folgress <75598739+Folgress@users.noreply.github.com>
2022-10-06 12:00:57 +0200

    added SCSS to compile routine

--- gulpfile.ts/compile.ts
+++ gulpfile.ts/compile.ts
@@ -1,11 +1,13 @@
 import GulpClient from 'gulp';
 import { devPathC, Paths } from './index';
 import { compilePUG } from './pug';
+import { compileSCSS } from './scss';
 
 function getPath(Path: string): devPathC {
     return { src: Paths.src + Path, dist: Paths.dist + Path };
 }
 
 const compile_SlimeIM_Helper_PUG = () => compilePUG(getPath(Paths.Pages.SlimeIM_Helper));
+const compile_SlimeIM_Helper_SCSS = () => compileSCSS(getPath(Paths.Pages.SlimeIM_Helper));
 
-export const compile_SlimeIM_Helper = GulpClient.parallel(compile_SlimeIM_Helper_PUG);
+export const compile_SlimeIM_Helper = GulpClient.parallel(compile_SlimeIM_Helper_PUG, compile_SlimeIM_Helper_SCSS);
