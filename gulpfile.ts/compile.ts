c0ac3772a4ff29781296832400f5622570b6e1ae
Folgress <75598739+Folgress@users.noreply.github.com>
2022-10-06 12:59:53 +0200

    added TypeScript compilation

--- gulpfile.ts/compile.ts
+++ gulpfile.ts/compile.ts
@@ -2,12 +2,14 @@ import GulpClient from 'gulp';
 import { devPathC, Paths } from './index';
 import { compilePUG } from './pug';
 import { compileSCSS } from './scss';
+import { compileTS } from './typescript';
 
 function getPath(Path: string): devPathC {
     return { src: Paths.src + Path, dist: Paths.dist + Path };
 }
 
+const compile_SlimeIM_Helper_TS = () => compileTS(getPath(Paths.Pages.SlimeIM_Helper));
 const compile_SlimeIM_Helper_PUG = () => compilePUG(getPath(Paths.Pages.SlimeIM_Helper));
 const compile_SlimeIM_Helper_SCSS = () => compileSCSS(getPath(Paths.Pages.SlimeIM_Helper));
 
-export const compile_SlimeIM_Helper = GulpClient.parallel(compile_SlimeIM_Helper_PUG, compile_SlimeIM_Helper_SCSS);
+export const compile_SlimeIM_Helper = GulpClient.parallel(compile_SlimeIM_Helper_TS, compile_SlimeIM_Helper_PUG, compile_SlimeIM_Helper_SCSS);
